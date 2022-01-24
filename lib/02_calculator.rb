b722a1f141b4440ff58158a6e5e208abb3251e46
alkobarii <a.alkobarii@gmail.com>
2022-01-24 21:11:00 +0100

    first commit

---
+++ lib/02_calculator.rb
@@ -0,0 +1,23 @@
+def add(num1, num2)
+    num1 + num2
+ end
+
+def subtract(num1,num2)
+    num1 - num2
+end
+
+def sum(array)
+    array.sum
+end
+
+def multiply(num1, num2)
+    num1 * num2
+end
+
+def power(x, y)
+    x ** y
+end
+
+def factorial (n) 
+    (2..n).reduce(1,:*)
+end
